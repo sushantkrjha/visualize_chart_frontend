23e4161c474d13b769a64997e99c1c182dce193b
alanchrissantony <alanchrissantony@gmail.com>
2024-08-22 23:39:47 +0530

    data fetched

--- src/components/ChartComponent.js
+++ src/components/ChartComponent.js
@@ -1,39 +1,54 @@
-// src/components/ChartComponent.js
 import React, { useEffect, useState } from 'react';
 import { Line, Bar } from 'react-chartjs-2';
-import { Chart as ChartJS, LineElement, CategoryScale, LinearScale, BarElement } from 'chart.js';
 import { fetchData } from '../utils/api';
+import {
+  Chart as ChartJS,
+  LineElement,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  PointElement,
+} from 'chart.js';
 
-ChartJS.register(LineElement, CategoryScale, LinearScale, BarElement);
+ChartJS.register(
+  LineElement,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  PointElement
+);
 
 const ChartComponent = ({ url, chartType = 'line', labels, datasets }) => {
   const [chartData, setChartData] = useState(null);
 
   useEffect(() => {
     const loadData = async () => {
-      const data = await fetchData(url);
-      if (data && Array.isArray(data)) {
+      const data = await fetchData(url)
+
+      console.log(data);
+      
+
+      if (data && Array.isArray(data.data)) {
+        const fetchedLabels = labels || data.data.map(item => item.city);
+        const fetchedData = datasets || [{
+          label: 'Customer Count',
+          data: data.data.map(item => item.customer_count),
+          borderColor: 'rgba(75,192,192,1)',
+          backgroundColor: 'rgba(75,192,192,0.2)',
+        }];
+
         setChartData({
-          labels: labels || data.map(item => item.label),
-          datasets: datasets || [
-            {
-              label: 'Dataset',
-              data: data.map(item => item.value),
-              borderColor: 'rgba(75,192,192,1)',
-              backgroundColor: 'rgba(75,192,192,0.2)',
-            },
-          ],
+          labels: fetchedLabels,
+          datasets: fetchedData,
         });
-      } else {
-        setChartData(null); // Handle undefined data
       }
     };
 
     loadData();
   }, [url, labels, datasets]);
 
-  if (!chartData) {
-    return <div>Loading...</div>; // Or any other fallback UI
+  if (!chartData || !chartData.labels || !chartData.datasets) {
+    return <div>Loading...</div>;
   }
 
   return (
